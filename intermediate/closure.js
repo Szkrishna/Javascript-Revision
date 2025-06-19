d6f5f693db51c0959f019bc06e84e1a9871ffd9d
Szkrishna <krisnas9792@gmail.com>
2025-06-19 16:47:23 +0530

    closure in JS

---
+++ intermediate/closure.js
@@ -0,0 +1,18 @@
+function a() {
+    const x = 3;
+    return function b() {
+        const y = 5;
+        const mul1 = x * y;
+        return function c(z) {
+            const mul2 = mul1 * z;
+            console.log(`Mutliply of ${x}, ${y} and ${z} is ${mul2}`);
+        }
+    }
+}
+
+const c1 = a();
+const c2 = c1();
+c2(2);
+c2(3);
+c2(4);
+c2(5);
